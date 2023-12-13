3cce6e28f98f36c8321c34f6ef7a369c65b1aa29
ydj1215 <ydjdj2@naver.com>
2023-12-13 17:43:33 +0900

    20231213 1743 ydj

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./components/Main";
 import { SignUp } from "./pages/member/SignUp";
 import { Login } from "./pages/member/Login";
-import { Home } from "./pages/Home";
-import { BuyReview } from "./pages/goods/BuyReview";
 import { Cart } from "./pages/goods/Cart";
 import { MyPage } from "./pages/member/MyPage";
-import  {GoodsList}  from "./pages/goods/GoodsList";
-import  {GoodsDetail}  from "./pages/goods/GoodsDetail";
-import { AuctionList } from "./pages/aution/AuctionList"; 
+import { GoodsList } from "./pages/goods/GoodsList";
+import { GoodsDetail } from "./pages/goods/GoodsDetail";
+import { AuctionList } from "./pages/aution/AuctionList";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/Signup" element={<SignUp />}></Route>
+          <Route path="/Signup" element={<SignUp />}></Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route element={<Main />}>
             {/* <Route path="/" element={<Home />}></Route>      */}
-            <Route path="/" element={<GoodsList/>}></Route>   
-            <Route path="/Goods/:num" element={<GoodsDetail/>}></Route>   
-            <Route path="/BuyReview" element={<BuyReview />}></Route>
+            <Route path="/" element={<GoodsList />}></Route>
+            <Route path="/Goods/:num" element={<GoodsDetail />}></Route>
             <Route path="/AuctionList" element={<AuctionList />}></Route>
             <Route path="/Cart" element={<Cart />}></Route>
             <Route path="/MyPage" element={<MyPage />}></Route>
