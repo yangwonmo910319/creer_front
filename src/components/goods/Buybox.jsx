f921c423a2d8079ad8207cca4ae5485557d748ea
ydj1215 <ydjdj2@naver.com>
2023-12-28 17:50:13 +0900

    231228 1749 ydj

--- src/components/goods/Buybox.jsx
+++ src/components/goods/Buybox.jsx
@@ -19,7 +19,7 @@ export const Buybox = ({ list, optionList, quantity1 }) => {
   const accessToken = localStorage.getItem("accessToken");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [content, setContent] = useState({});
-  const [page, setPage] = useState('');
+  const [page, setPage] = useState("");
 
   //판매자 Pk
   const [goodsDetailId, setGoodsDetailId] = useState("");
@@ -40,9 +40,10 @@ export const Buybox = ({ list, optionList, quantity1 }) => {
       quantity: quantity1, //수량
       title: list.goodsTitle, //상품 제목
       goodsImg: list.goodsPic, //상품 이미지
-      price: list.goodsPrice,  //가격
+      price: list.goodsPrice, //가격
     });
   }, [option, status, quantity1, list]);
+
   // 장바구니 담기
   const cartAdd = async () => {
     try {
@@ -51,7 +52,7 @@ export const Buybox = ({ list, optionList, quantity1 }) => {
       if (res && res.status === 200) {
         console.log("장바구니에 물품을 담았습니다.");
         console.log(res.data);
-        setPage(res.data)
+        return res.data;
       } else {
         console.error("장바구니에 물품을 담는데 실패했습니다.", res);
         alert("장바구니에 물품을 담는데 실패했습니다.");
@@ -89,12 +90,12 @@ export const Buybox = ({ list, optionList, quantity1 }) => {
     }
   }, [optionList]);
 
-  // 구매 클릭 
-  useEffect(() => {
-    navigate(`/Goods/Payment/${page}`);
-  }, [page])
-  const purchaseClick = () => {
-    cartAdd();
+  // 구매 클릭
+  const purchaseClick = async () => {
+    const res = await cartAdd();
+    if (res) {
+      navigate(`/Goods/Payment/${res}`);
+    }
   };
 
   // 장바구니 클릭
@@ -106,6 +107,7 @@ export const Buybox = ({ list, optionList, quantity1 }) => {
     }
     setIsModalOpen(true);
   };
+
   // 채팅
   const CreateChatRoom = async () => {
     const accessToken = localStorage.getItem("accessToken");
